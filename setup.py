bb4a86a4395ae131ec515a8f5e49fa090e44ad1d
liu liu <leoliu@dhcp-10-105-82-214.wireless.northwestern.private>
2015-11-19 14:57:49 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='neuro_athelets',
+        version='0.1',
+        description='python ml library for neuro eeg data',
+        url='http://github.com/leolincoln/neuro_athelets',
+        author='Liu Liu',
+        author_email='leoliu@u.northwestern.edu',
+        license='MIT',
+        packages=['neuro_athelets'],
+        zip_safe=False)
